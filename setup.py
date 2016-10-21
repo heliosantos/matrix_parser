3cd3d17812fc1fb30c3dc5b69f18af88b58d8097
Helio Santos <heliosantos99@gmail.com>
2016-10-21 23:14:31 +0100

    add manifest to include licence file to package distribution

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='matrix_parser',
-    version='1.0',
+    version='1.0.1',
     description='A matrix shaped text (e.g csv) parser',
     long_description=readme,
     author='Hélio Santos',
